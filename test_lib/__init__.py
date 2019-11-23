0a91de4eeaa9a094c004594efe7726f9d0397bdd
harupy <hkawamura0130@gmail.com>
2019-11-23 22:01:25 +0900

    Fix doctest

---
+++ test_lib/__init__.py
@@ -0,0 +1,26 @@
+
+def hello_world():
+    """
+    Returns "hello world"
+
+    Example:
+    >>> hello_world()
+    'hello world'
+    """
+    return "hello world"
+
+
+def foo():
+    """
+    Returns "foo"
+
+    Example:
+    >>> foo()
+    'foo'
+    """
+    return "foo"
+
+
+if __name__ == '__main__':
+    print(hello_world())
+    print(foo())
