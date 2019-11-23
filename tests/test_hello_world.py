68c1efa68a918889417d73d05af0d9753c01a035
harupy <hkawamura0130@gmail.com>
2019-11-23 21:23:27 +0900

    add test

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,5 +1,9 @@
-from test_lib import hello_world
+import test_lib
 
 
 def test_hello_world():
-    assert hello_world() == "hello world"
+    assert test_lib.hello_world() == "hello world"
+
+
+def test_foo():
+    assert test_lib.foo() == "foo"
